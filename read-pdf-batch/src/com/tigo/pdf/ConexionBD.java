c1fb6b39d0f47fba6872a61b3f713f36fc8420db
altiuz4-ext Externo <altiuz4-ext@computec.com>
2020-08-17 14:03:15 -0500

    Mensaje eerror

--- read-pdf-batch/src/com/tigo/pdf/ConexionBD.java
+++ read-pdf-batch/src/com/tigo/pdf/ConexionBD.java
@@ -11,7 +11,12 @@ public class ConexionBD {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try {
-			//Class.forName("COM.ibm.db2.jdbc.app.DB2Driver");
+			//Class.forName("COM.ibm.db2os390.sqlj.jdbc.DB2SQLJDriver");
+			/** Estado: Fallo:Fallo de la prueba: [jcc][t4][2030][11211][3.59.81] A communication error occurred during operations on the connection's underlying socket, socket input stream, 
+or socket output stream.  Error location: Reply.fill() - insufficient data (-1).  Message: Insufficient data. ERRORCODE=-4499, SQLSTATE=08001**/
+			
+			/** [jcc][t4][2030][11211][4.25.13] A communication error occurred during operations on the connection's underlying socket, socket input stream, 
+or socket output stream.  Error location: Reply.fill() - insufficient data (-1).  Message: Insufficient data. ERRORCODE=-4499, SQLSTATE=08001**/
 			Class.forName("com.ibm.db2.jcc.DB2Driver");
 		} catch (ClassNotFoundException e) {
 			System.out.println("Please include Classpath  Where your DB2 Driver is located");
@@ -24,7 +29,7 @@ public class ConexionBD {
 		ResultSet rset = null;
 		boolean found = false;
 		 String url = 
-			     "jdbc:db2://jdbc:db2:desadbfacturaelectronica.database.windows.net:1433/desadbfacturaelectronica" +
+			     "jdbc:db2://desadbfacturaelectronica.database.windows.net:1433/desadbfacturaelectronica" +
 			     ":user=user_fe;password=user_fe;" +
 			     "traceLevel=" +
 			     (com.ibm.db2.jcc.DB2BaseDataSource.TRACE_ALL) + ";";
